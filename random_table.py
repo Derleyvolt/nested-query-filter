56032c2369454a8f23a8677c3aca7b24388e8df9
Wanderley <100Palavr@gmail.com>
2024-06-10 22:21:56 -0300

    Update random_table.py

--- random_table.py
+++ random_table.py
@@ -1,30 +1,14 @@
-import random
-
-# random.seed(42)
-
-def _random_between(min, max):
-    return random.randint(min, max)
-
-def _create_random_name(min, max):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    ret = []
-
-    len = _random_between(min, max)
-
-    for _ in range(len):
-        ret.append(random.choice(alphabet))
-
-    return "".join(ret)
+from random_services import *
 
 def create_random_table(number_records):
     table = []
 
     for i in range(number_records):
         table.append({
-            "name": _create_random_name(5, 10),
-            "age": str(_random_between(10, 99)),
-            "height": str(_random_between(1, 2) + random.random()),
-            "date": "{0}/{1}/{2}".format(_random_between(1, 31), _random_between(1, 12), _random_between(1980, 2050))
+            "name": random_service.get_random_string(5, 10),
+            "age": random_service.get_random_intenger_between(1, 1000),
+            "height": random_service.get_random_decimal_between(1, 1000),
+            "birth_day": random_service.get_random_date(),
         })
 
     return table
